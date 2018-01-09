8f05c1fdcede7012298695fe859c57da7af5752e
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-01-09 12:37:25 -0500

    Connected action and outlet from storyboard to view controller.

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guessedNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func checkGuess(_ sender: Any) {
+        
+        print("A guess was made.")
+        print(guessedNumber.text)
+        
+    }
+    
 }
 
