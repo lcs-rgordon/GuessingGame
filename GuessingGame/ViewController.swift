64d7505d24c664d65abecec21d6302e2cde90175
Russell Gordon <rgordon@lcs.on.ca>
2018-01-15 08:37:00 -0500

    Takes user input and checks that it is an integer

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
     @IBOutlet weak var guessedNumber: UITextField!
+    @IBOutlet weak var instructionsLabel: UILabel!
     
+    // MARK: Properties
+    let targetNumber = Int(arc4random_uniform(1001)) // Produce a number between 1 and 1000, inclusive (but not 1001)
+    
+    // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,10 +28,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: New methods
     @IBAction func checkGuess(_ sender: Any) {
         
         print("A guess was made.")
         print(guessedNumber.text)
+        
+        // Unwrap the optional
+        guard let providedInput = guessedNumber.text else {
+            return // Could not unwrap optional
+        }
+        
+        // Now try to create an integer
+        guard let guess = Int(providedInput) else {
+            instructionsLabel.text = instructionsLabel.text! + "\n\nA number, please."
+            return
+        }
         
     }
     
