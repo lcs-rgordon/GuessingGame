3a1a0ef1362ad01e6d3d4405b84a7ebace2c8e6b
Russell Gordon <rgordon@lcs.on.ca>
2018-01-15 09:01:52 -0500

    Implemented ability to play again

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -13,14 +13,17 @@ class ViewController: UIViewController {
     // MARK: Outlets
     @IBOutlet weak var guessedNumber: UITextField!
     @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var guessButton: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
     
     // MARK: Properties
-    let targetNumber = Int(arc4random_uniform(1001)) // Produce a number between 1 and 1000, inclusive (but not 1001)
+    var targetNumber : Int = -1
     
     // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startNewGame(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,13 +50,39 @@ class ViewController: UIViewController {
         
         // Check the guess
         if guess == targetNumber {
-            instructionsLabel.text = "You got it!"
+            endGame()
         } else if guess > targetNumber {
             instructionsLabel.text = "Guess lower."
         } else {
             instructionsLabel.text = "Guess higher."
         }
         
+    }
+    
+    // End the game and ask whether user wants to play again
+    func endGame() {
+        instructionsLabel.text = "You got it!"
+        guessButton.isHidden = true
+        playAgainButton.isHidden = false
+    }
+    
+    // Start a new game
+    @IBAction func startNewGame(_ sender: Any) {
+        
+        // Generate a number for the player to guess
+        self.targetNumber = Int(arc4random_uniform(1001)) // Produce a number between 1 and 1000, inclusive (but not 1001)
+        
+        // Reset the interface
+        playAgainButton.isHidden = true
+        guessButton.isHidden = false
+        instructionsLabel.text = "I'm thinking of a number\nbetween 0 and 1000.\n\nGuess it!"
+        guessedNumber.text = ""
+        
+    }
+    
+    // User pressed return so make the the same as pressing the button
+    @IBAction func inputGiven(_ sender: Any) {
+        checkGuess(self)
     }
     
 }
